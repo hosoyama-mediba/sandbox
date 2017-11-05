7ed2e6aef547c1325af119bebb1ec8ef91bc5df0
Terunobu Hosoyama <hosoyama@gmail.com>
2017-11-05 16:40:07 +0900

    add tab-ui

---
+++ docs/tab-ui/app.js
@@ -0,0 +1,160 @@
+class News {
+    /**
+     * @property {string} BLOCK - ブロック
+     */
+    static get BLOCK() {
+        return 'News';
+    }
+
+    /**
+     * コンストラクタ
+     *
+     * @param {Object} classes - エレメント定義
+     */
+    constructor(classes) {
+        this.el = document.querySelector(`.${News.BLOCK}`);
+        this.tabBar = this.el.querySelector(`.${News.BLOCK}__${classes.tabBar}`);
+        this.tabBarInner = this.tabBar.querySelector(`.${News.BLOCK}__${classes.tabBarInner}`);
+        this.tabDecorator = this.tabBar.querySelector(`.${News.BLOCK}__${classes.tabDecorator}`);
+        this.tabList = this.tabBarInner.querySelector(`.${News.BLOCK}__${classes.tabList}`);
+        this.tabItems = this.tabList.querySelectorAll(`.${News.BLOCK}__${classes.tabItems}`);
+        this.tabPanelList = this.el.querySelector(`.${News.BLOCK}__${classes.tabPanelList}`);
+        this.tabPanelItems = this.tabPanelList.querySelectorAll(`.${News.BLOCK}__${classes.tabPanelItems}`);
+    }
+
+    /**
+     * 初期化
+     */
+    initialize() {
+        const tabItems = this.tabItems;
+        const defaultTabItem = Array.from(tabItems).find(tabItem => tabItem.classList.contains('-active'));
+
+        this.initializeTabDecorator(defaultTabItem.offsetLeft, defaultTabItem.offsetWidth);
+        this.initializeTabItems();
+    }
+
+    /**
+     * マーカーを初期化
+     *
+     * @param {Number} left - X座標
+     * @param {Number} wdith - 横幅
+     */
+    initializeTabDecorator(left, width) {
+        const tabDecorator = this.tabDecorator;
+
+        this.transformDecorator(left, width);
+
+        // アニメーションしないようにタイミングをずらす
+        requestAnimationFrame(() => {
+            tabDecorator.classList.add('-ready');
+        });
+    }
+
+    /**
+     * タブを初期化(イベントハンドラをバインド)
+     */
+    initializeTabItems() {
+        const tabItems = this.tabItems;
+        Array.from(tabItems).forEach(tabItem => {
+            tabItem.addEventListener('click', e => this.onClickTabItem(e));
+        });
+    }
+
+    /**
+     * マーカーの移動
+     *
+     * @param {Number} left - X座標
+     * @param {Number} wdith - 横幅
+     */
+    transformDecorator(left, width) {
+        this.tabDecorator.style.transform = `translateX(${left}px) scaleX(${width})`;
+    }
+
+    /**
+     * タブをアクティブにする
+     *
+     * @param {Number} index - インデックス
+     */
+     selectActiveTabItem(index) {
+        Array.from(this.tabItems).forEach((tabItem, tabItemIndex) => {
+            if (tabItemIndex === index) {
+                tabItem.classList.add('-active');
+            } else {
+                tabItem.classList.remove('-active');
+            }
+        });
+    }
+
+    /**
+     * タブパネルをアクティブにする
+     *
+     * @param {Number} index - インデックス
+     */
+    selectActiveTabPanelItem(index) {
+        Array.from(this.tabPanelItems).forEach((tabPanelItem, tabPanelItemIndex) => {
+            if (tabPanelItemIndex === index) {
+                tabPanelItem.classList.add('-active');
+            } else {
+                tabPanelItem.classList.remove('-active');
+            }
+        });
+    }
+
+    /**
+     * 指定したタブが真ん中に来るように横スクロール
+     *
+     * @param {Element} tabItem - タブ
+     */
+    scrollTabBarInner(tabItem) {
+        const tabBarInner = this.tabBarInner;
+        let distance = (tabItem.offsetLeft + (tabItem.offsetWidth / 2)) - tabBarInner.scrollLeft - (window.innerWidth / 2);
+        const maxScrollLeft = distance > 0 ? tabBarInner.scrollWidth - window.innerWidth : 0;
+        const expectedScrollLeft = tabBarInner.scrollLeft + distance + (tabItem.scrollHeight / 2);
+        if ((distance > 0 && expectedScrollLeft > maxScrollLeft) || (distance < 0 && expectedScrollLeft < maxScrollLeft)) {
+            distance = maxScrollLeft - tabBarInner.scrollLeft;
+        }
+
+        const start = tabBarInner.scrollLeft;
+        const duration = 300;
+        const delay = 0;
+        const max = tabBarInner.scrollWidth - window.innerWidth;
+        let timeStart, timeElapsed;
+
+        requestAnimationFrame((time) => {
+            timeStart = time;
+            loop(time);
+        });
+
+        function loop(time) {
+            timeElapsed = time - timeStart;
+
+            const scrollLeft = easeOutExpo(timeElapsed, start, distance, duration);
+            tabBarInner.scrollLeft = scrollLeft;
+
+            if (timeElapsed < duration) {
+                requestAnimationFrame(loop)
+            } else {
+                tabBarInner.scrollLeft = start + distance;
+            }
+        }
+
+        function easeOutExpo(t, b, c, d) {
+            return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
+        }
+    }
+    /**
+     * タブがクリックされたとき
+     *
+     * @param {Event} e - イベント
+     */
+    onClickTabItem(e) {
+        const tabBarInner = this.tabBarInner;
+        const tabItem = e.currentTarget;
+        const tabItemIndex = Array.from(this.tabItems).indexOf(tabItem);
+
+        this.transformDecorator(tabItem.offsetLeft, tabItem.offsetWidth);
+        this.selectActiveTabItem(tabItemIndex);
+        this.selectActiveTabPanelItem(tabItemIndex);
+        this.scrollTabBarInner(tabItem);
+    }
+}
